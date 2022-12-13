65bb9d03b3aa0fba9677dd50d2e6cce57ea2c280
Anant Kumawat <anantkumawat22@gmail.com>
2022-12-13 15:36:17 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -14,7 +15,20 @@ class MyApp extends StatelessWidget {
   String name = "Anant";
 
     return MaterialApp(
-      home: HomePage()
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: '/home',
+      routes: {
+        "/" :(context) => LoginPage(),
+        "/home" :(context) => HomePage(),
+        "/login" :(context) => LoginPage(),
+      },
     );
   }
 }
