f837851264305a63dcad4773b0c2275060b98f04
Anant Kumawat <anantkumawat22@gmail.com>
2022-12-14 17:02:58 +0530

    Day 9 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 void main(){
@@ -18,10 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        // fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: MyTheme.LightTheme(context),
       darkTheme: ThemeData(
         brightness: Brightness.dark
       ),
