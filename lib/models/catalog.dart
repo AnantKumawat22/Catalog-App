796aff73e2f42c27cf0d81ffff63e3cfbcbb330b
Anant Kumawat <anantkumawat22@gmail.com>
2022-12-15 14:17:43 +0530

    Day 10 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,16 @@
+class CatalogModel{
+  static final items = [Item(
+    id: 1,
+    name: "iphone 12 pro",
+    desc: "Apple iphone 12th generation.",
+    price: 999,
+    color: "#33505a",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
+  )];
+}
+
 class Item{
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final int price;
@@ -9,11 +20,3 @@ class Item{
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products = [Item(
-  id: "Iphone12",
-  name: "iphone 12 pro",
-  desc: "Apple iphone 12th generation.",
-  price: 100000,
-  color: "#33505a",
-  image: ""
-)];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import '../models/catalog.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+
     return Scaffold(
         appBar: AppBar(
-          title: Text("Catalog App"),
+          title: const Text("Catalog App"),
         ),
-        body: Center(
-            child: Container(
-              child: Text("Welcome to 30 days of flutter.")
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+              itemCount: CatalogModel.items.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(item: CatalogModel.items[index]);
+              }
+          ),
         ),
-        drawer: MyDrawer()
+        drawer: const MyDrawer()
     );
   }
 }
